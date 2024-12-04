eb4403dd7c89f976239f2fe185974544e73ae333
solomongrad <solomongrad@gmail.com>
2024-12-04 11:03:19 +0300

    сделал импорт из json файла

--- backend/foodgram/recipes/management/commands/import_ingredients_json.py
+++ backend/foodgram/recipes/management/commands/import_ingredients_json.py
@@ -17,8 +17,9 @@ class Command(BaseCommand):
         file_path = os.path.join(settings.IMPORTING_FILES_DIR,
                                  'ingredients.json')
         with open(file_path, mode='r', encoding='utf-8') as file:
-            models_list = [Ingredients(name=row['name'],
-                                       measurement_unit=row['measurement_unit'])
+            models_list = [Ingredients(
+                name=row['name'], measurement_unit=row['measurement_unit']
+            )
                            for row in json.load(file)]
             Ingredients.objects.bulk_create(models_list, ignore_conflicts=True)
 
